6d7ee1dd228cc0d619dd5855dfbc4a871f1428ff
Russell Howell <rhowell1110@gmail.com>
2017-09-05 22:03:33 -0400

    Dev: retrieval of timetable data generalized to accomodate all triptypes & locations in JSON file

--- src/providers/timetable/timetable.ts
+++ src/providers/timetable/timetable.ts
@@ -13,7 +13,7 @@ export class TimetableProvider {
 timetable: any[];
 
   constructor(private http: Http) {
-    console.log('Enter TimetableProvider Provider');
+    console.log('Enter TimetableProvider');
     this.timetable = [];
     this.loadTimetable();
 }
@@ -25,44 +25,36 @@ loadTimetable(){
       .map((res:Response) => res.json().timetable).subscribe((timetableData) => {
 
         //load JSON timetable into momentJS time objects
-        for (const key of Object.keys(timetableData["on_campus_weekday"])) { //TODO - generalize this to all triptypes
-            
+        for (const key0 of Object.keys(timetableData)){
+          this.timetable[key0] = [];
+          for (const key1 of Object.keys(timetableData[key0])) {
 
             let tmp = [];
             let morningIndex = 0; //keep track of location of first non-early morning time in array
 
-
-            timetableData["on_campus_weekday"][key].forEach((time) => {
-
+            timetableData[key0][key1].forEach((time) => {
               let newTime = moment(time, 'HH:mm');
 
-              if (newTime < moment('01:30', 'HH:mm'))
-            {
-              //if the time is before 01:30 add it to the beginning of the time array
-              //and at the end of the array but with one day added, increment index to keep track of beginning of array
-              tmp.splice(morningIndex, 0, moment(newTime, 'HH:mm'));
-              morningIndex++;
-              tmp.push(moment(newTime,'HH:mm').add(1,'days'));
-            }
-            else
-            {
-              tmp.push(moment(newTime,'HH:mm'));
-            }
+              if (newTime < moment('01:30', 'HH:mm')){
+                //if the time is before 01:30 add it to the beginning of the time array
+                //and at the end of the array but with one day added, increment index
+                //in order to keep track of begining of array
+                tmp.splice(morningIndex, 0, moment(newTime, 'HH:mm'));
+                morningIndex++;
+                tmp.push(moment(newTime,'HH:mm').add(1,'days'));
+              }
+              else{
+                tmp.push(moment(newTime,'HH:mm'));
+              }
 
             });
 
-
-            this.timetable[key] = tmp;
+            this.timetable[key0][key1] = tmp;
+          }
         }
       });
 
-
 }
 
-// //modify to return specific timetable
-// getData() {
-//   return this.http.get("../../assets/timetable.json")
-//       .map((res:Response) => res.json().timetable);
-// }
 
 }
