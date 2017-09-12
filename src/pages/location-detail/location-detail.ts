1f49ff3a16647554bf6e2e7d50714f5ebb839af0
Russell Howell <rhowell1110@gmail.com>
2017-09-12 15:52:15 -0400

    Dev: removed unecessary import from LocationDetailPage

--- src/pages/location-detail/location-detail.ts
+++ src/pages/location-detail/location-detail.ts
@@ -1,4 +1,4 @@
-import { Component, ViewChild } from '@angular/core';
+import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { TimetableProvider } from '../../providers/timetable/timetable'
 import  moment  from 'moment';
